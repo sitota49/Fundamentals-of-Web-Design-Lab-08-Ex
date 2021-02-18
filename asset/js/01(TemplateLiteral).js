66132d586fbc9e0fa14113f6e346cc07aaa2927d
sitota49 <se.sitota.ezra@gmail.com>
2021-02-18 09:17:57 +0300

    Template Literal

--- asset/js/01(TemplateLiteral).js
+++ asset/js/01(TemplateLiteral).js
@@ -19,9 +19,9 @@ function templateStringConcatenation() {
 
     //1. Create a variable called fullIntroTemplate and Store the same string displayed above with template literal
 
-
+    var fullIntroTemplate = `My name is ${student.name} and I am ${student.age} years old.`
     //3. Display it [change it to fullIntroTemplate]
-    templateConcatenationResult.innerHTML = fullIntro;
+    templateConcatenationResult.innerHTML = fullIntroTemplate;
 
 }
 
@@ -33,9 +33,10 @@ function displayTemplateExpression() {
 
     //1. Create a variable called result1 and Store the same string displayed above with template literal
 
+    const result1 = `The Sum of the Numbers is  :  ${sum(1, 2)} `;
 
     //3. Display it [change it to result1]
-    templateExpression.innerHTML = result;
+    templateExpression.innerHTML = result1;
 
 
 
