27b10ec1def96d3dca596e30f3aa30da798bf4e6
sitota49 <se.sitota.ezra@gmail.com>
2021-02-18 10:24:39 +0300

    OOP3

--- asset/js/05(OOP3).js
+++ asset/js/05(OOP3).js
@@ -1,16 +1,24 @@
 // 1. Create a Person_ES6 Class 
-
-
-
 //2. Create the constructor that initialize firstName and lastName
-
-
 //3. Define greeting() method that displays Full Name
+
+class Person_ES6 {
+
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    greeting() {
+        return (`${this.firstName} ${this.lastName}`);
+    }
+
+}
+
 
 
 //4. Create  Object using ES6 class by passing firstName,lastName ["Bran Stark"]
 
-
+const Bran = new Person_ES6('Bran', 'Stark')
 
 //5. Display the greeting on Bran Object [remove the string when you have the object]
-Obj_C3.innerHTML = "Bran.greeting()";
+Obj_C3.innerHTML = Bran.greeting();
