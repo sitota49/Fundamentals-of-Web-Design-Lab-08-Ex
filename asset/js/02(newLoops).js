d430b4d00147c696e23629aed910a95a04f4d622
sitota49 <se.sitota.ezra@gmail.com>
2021-02-18 09:26:35 +0300

    New Loops

--- asset/js/02(newLoops).js
+++ asset/js/02(newLoops).js
@@ -10,12 +10,16 @@ document.addEventListener('DOMContentLoaded', () => {
 function forINLoop() {
 
     // A Student Object 
-    const student = { name: "Abebe", age: 21, year: "2dn year" }
+    const student = { name: "Abebe", age: 21, year: "2nd year" }
 
     // 1. Create a for in loop that iterate over the student object
-
-
     // 2. Inside the loop paint the UI [Use the Format Given Below]
+
+    for (Key in student) {
+        Value = student[Key]
+        forInLoop.innerHTML += `
+             <li class="list-group-item" >${Key} : ${Value}</li>`;
+    }
 
 
     //Use this Format 
@@ -35,9 +39,15 @@ function forOFLoop() {
     const students = [{ name: "Abebe", age: 21, year: "2dn year" }, { name: "Kebede", age: 22, year: "2dn year" }, { name: "Abebech", age: 23, year: "2dn year" }]
 
     // 1. Create a for of loop that iterate over the students Array and displays the names of students 
-
-
     // 2. Inside the loop paint the UI [Use the Format Given Below]
+
+    for (student of students) {
+        forOfLoop.innerHTML +=
+            `
+             <li class="list-group-item" > Name: ${student.name}</li>
+    
+         `
+    }
 
 
     //Use this Format 
