99477fa13a592e3b9b37b994c920a6d45b958e48
sitota49 <se.sitota.ezra@gmail.com>
2021-02-18 09:42:57 +0300

    Destrucutring

--- asset/js/03(Destructuring).js
+++ asset/js/03(Destructuring).js
@@ -18,17 +18,19 @@ function arrayDestructuring() {
 
     //1. Destructor this array to firstName and lastName
 
+    let [firstName, lastName] = arr;
+
     //2. Display it  [Replace it with ${firstName} ${lastName} once you have the variables]
-    des_arr.innerHTML = `Hello , I am firstName lastName `
+    des_arr.innerHTML = `Hello , I am ${firstName} ${lastName} `
 
 
     // we have an array with the name and surname
     let str = "Margaery Tyrell"
 
     //1. Destructor this string array to firstName and lastName by using split() method of string object
-
+    let [firstname, lastname] = str.split(' ');
     //2. Display it [Replace it with ${firstName} ${lastName} once you have the variables]
-    des_string.innerHTML = `Hello , I am firstName lastName `
+    des_string.innerHTML = `Hello , I am ${firstname} ${lastname} `
 
 
 
@@ -42,15 +44,15 @@ function objectDestructuring() {
     let person = { firstName: "John", lastName: "Smith" }
 
     //1. Destructor this object to firstName and lastName 
-
+    let { firstName, lastName } = person;
     //2. Display it [Replace it with ${firstName} ${lastName} once you have the variables]
-    des_obj1.innerHTML = `Hi , I am firstName lastName `
+    des_obj1.innerHTML = `Hi , I am ${firstName} ${lastName} `
 
 
     //1. Destructor this object to fName and sName [different naming]
-
+    let { firstName: fName, lastName: lName } = person;
     //2. Display it [Replace it with ${Name} ${lName} once you have the variables]
-    des_obj2.innerHTML = `Hi , I am fName lName `
+    des_obj2.innerHTML = `Hi , I am ${fName} ${lName} `
 
 
 }
@@ -101,11 +103,11 @@ function sum1(x1, x2, x3, x4) {
 
 
 //3. Make this function to support rest, and add 5 numbers
-function sum2(x1, x2) {
+function sum2(x1, x2, ...rest) {
 
     //4. Function that add 5 numbers [Consider Adding the arr[0], arr[1], arr[2]]
 
-    return x1 + x2;
+    return x1 + x2 + rest[0] + rest[1] + rest[2];
 
 
 }
