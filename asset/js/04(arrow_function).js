4a3c3d605822f5a738d3ceea80a5a02b96db9a3a
sitota49 <se.sitota.ezra@gmail.com>
2021-02-18 09:46:57 +0300

    Arrow function

--- asset/js/04(arrow_function).js
+++ asset/js/04(arrow_function).js
@@ -2,8 +2,8 @@
 document.addEventListener('DOMContentLoaded', () => {
 
 
-    arrow_full.innerHTML = `The Sum is :  ${arrowFun1(1,2)}`;
-    arrow_red1.innerHTML = `The Sum is :  ${arrowFun2(1,2)}`;
+    arrow_full.innerHTML = `The Sum is :  ${arrowFun1(1, 2)}`;
+    arrow_red1.innerHTML = `The Sum is :  ${arrowFun2(1, 2)}`;
     arrow_red2.innerHTML = `The Sqrt is :  ${arrowFun3(4)}`;
 
 
@@ -13,11 +13,13 @@ document.addEventListener('DOMContentLoaded', () => {
 //1. Replace the empty string with arrow function 
 
 //2. An arrow function with full syntax
-const arrowFun1 = '';
+const arrowFun1 = (x, y) => {
+    return x + y
+};
 
 //3. A minimized arrow function [No return statement , no curly brace]
-const arrowFun2 = '';
+const arrowFun2 = (x, y) => x + y;
 
 //4. A minimized arrow function [No return statement , no curly brace, no square bracket]
 //5. The function will return the sqrt , use Math.sqrt(var);
-const arrowFun3 = '';
+const arrowFun3 = x => Math.sqrt(x);
